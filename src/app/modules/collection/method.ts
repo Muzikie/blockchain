8112d99411877a810f0264ee343407af8321f9e4
Ali Haghighatkhah <ali@lightcurve.io>
2022-11-29 12:14:29 +0100

    Use the controllers to implement getAccount and getCollection methods

--- src/app/modules/collection/method.ts
+++ src/app/modules/collection/method.ts
@@ -1,3 +1,27 @@
 import { BaseMethod } from 'lisk-sdk';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ModuleEndpointContext } from 'lisk-framework';
+import {
+  CollectionAccountJSON,
+  CollectionJSON,
+  CollectionAccount,
+  Store,
+  Collection,
+} from './types';
+import { CollectionAccountStore } from './stores/collectionAccount';
+import { CollectionStore } from './stores/collection';
+import { getAccount, getAudio } from './controllers';
 
-export class CollectionMethod extends BaseMethod {}
+export class CollectionMethod extends BaseMethod {
+  // Get account by address
+  public async getAccount(context: ModuleEndpointContext): Promise<CollectionAccountJSON> {
+    const collectionAccountSubStore = this.stores.get(CollectionAccountStore);
+    return getAccount(context, collectionAccountSubStore as Store<CollectionAccount>)
+  }
+
+  // Get Collection by collectionID
+  public async getAudio(context: ModuleEndpointContext): Promise<CollectionJSON> {
+    const collectionSubStore = this.stores.get(CollectionStore);
+    return getAudio(context, collectionSubStore as Store<Collection>);
+  }
+}
