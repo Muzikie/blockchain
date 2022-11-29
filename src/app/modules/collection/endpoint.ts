f287626d53060236cbc87d8aa5fc2750419865bd
Ali Haghighatkhah <ali@lightcurve.io>
2022-11-29 10:55:42 +0100

    Implement getCollection endpoint

--- src/app/modules/collection/endpoint.ts
+++ src/app/modules/collection/endpoint.ts
@@ -2,11 +2,13 @@ import { BaseEndpoint, codec } from 'lisk-sdk';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ModuleEndpointContext } from 'lisk-framework';
 import { address as cryptoAddress } from '@liskhq/lisk-cryptography';
-import { CollectionAccountJSON } from './types';
-import { accountStoreSchema } from './schemas';
+import { CollectionAccountJSON, CollectionJSON } from './types';
+import { accountStoreSchema, collectionStoreSchema } from './schemas';
 import { CollectionAccountStore } from './stores/collectionAccount';
+import { CollectionStore } from './stores/collection';
 
 export class CollectionEndpoint extends BaseEndpoint {
+  // Get account by address
   public async getAccount(context: ModuleEndpointContext): Promise<CollectionAccountJSON> {
     const collectionAccountSubStore = this.stores.get(CollectionAccountStore);
     const { address } = context.params;
@@ -37,5 +39,37 @@ export class CollectionEndpoint extends BaseEndpoint {
     );
     const accountJSON: CollectionAccountJSON = codec.toJSON(accountStoreSchema, accountData);
     return accountJSON;
+  }
+
+  // Get Collection by collectionID
+  public async getAudio(context: ModuleEndpointContext): Promise<CollectionJSON> {
+    const collectionSubStore = this.stores.get(CollectionStore);
+    const { collectionID } = context.params;
+
+    let query: Buffer;
+
+    if (Buffer.isBuffer(collectionID)) {
+      query = collectionID;
+    } else if (typeof collectionID === 'string') {
+      query = Buffer.from(collectionID, 'hex');
+    } else {
+      throw new Error('Parameter collectionID must be a string or a buffer.');
+    }
+
+    const collectionExists = await collectionSubStore.has(
+      context,
+      query,
+    );
+
+    if (!collectionExists) {
+      throw new Error(`No collection with id ${query.toString('hex')} found.`);
+    }
+
+    const collectionData = await collectionSubStore.get(
+      context,
+      query,
+    );
+    const collectionJSON: CollectionJSON = codec.toJSON(collectionStoreSchema, collectionData);
+    return collectionJSON;
   }
 }
