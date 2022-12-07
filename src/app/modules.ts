296bff564b9bdaeba9b32a965640b4e6fade2e05
Ali Haghighatkhah <ali@lightcurve.io>
2022-12-07 15:06:17 +0100

    Add token depdendency

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { TokenModule } from 'lisk-framework';
 import { AudioModule } from "./modules/audio/module";
 import { CollectionModule } from "./modules/collection/module";
 import { SubscriptionModule } from "./modules/subscription/module";
@@ -7,10 +9,13 @@ import { SubscriptionModule } from "./modules/subscription/module";
 export const registerModules = (app: Application): void => {
   const audioModule = new AudioModule();
   const collectionModule = new CollectionModule();
+  const subscriptionModule = new SubscriptionModule();
+  const tokenModule = new TokenModule();
 
   audioModule.addDependencies(collectionModule.method);
+  subscriptionModule.addDependencies(tokenModule.method);
 
   app.registerModule(audioModule);
   app.registerModule(collectionModule);
-  app.registerModule(new SubscriptionModule());
+  app.registerModule(subscriptionModule);
 };
