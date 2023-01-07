0610155076248b8b83577b617467e58d042d94b4
Ali Haghighatkhah <ali@lightcurve.io>
2023-01-07 22:10:40 +0100

    Add hash and meta to collection

--- src/app/modules/collection/commands/set_attributes_command.ts
+++ src/app/modules/collection/commands/set_attributes_command.ts
@@ -58,6 +58,8 @@ export class SetAttributesCommand extends BaseCommand {
     // Audios of a collection can be changes from the audio module
     const updatedObject: Collection = {
       ...params,
+      hash: collectionNFT.hash,
+      meta: collectionNFT.meta,
       audios: collectionNFT.audios,
       creatorAddress: collectionNFT.creatorAddress,
     };
