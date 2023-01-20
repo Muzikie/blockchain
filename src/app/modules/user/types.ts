c4742d7e68f383e2053cb8264477675bc94f793b
Ali Haghighatkhah <ali@lightcurve.io>
2023-01-20 13:24:05 +0100

    Create user store

---
+++ src/app/modules/user/types.ts
@@ -0,0 +1,23 @@
+export enum SocialAccountType {
+  Instagram = 0,
+  Twitter = 1,
+  Youtube = 2,
+}
+
+interface SocialAccount {
+  username: string;
+  type: SocialAccountType;
+}
+
+export interface User {
+  name: string;
+  nickName: string;
+  description: string;
+  socialAccounts: SocialAccount[];
+  avatarHash: Buffer;
+  avatarSignature: Buffer;
+  bannerHash: Buffer;
+  bannerSignature: Buffer;
+  creatorAddress: Buffer;
+}
+
