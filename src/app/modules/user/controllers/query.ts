332675e26e1a075f397fd6da3227bdfdc5eae2db
Ali Haghighatkhah <ali@lightcurve.io>
2023-01-20 16:49:47 +0100

    Add getUser endpoint

--- src/app/modules/user/controllers/query.ts
+++ src/app/modules/user/controllers/query.ts
@@ -2,8 +2,14 @@ import { codec } from 'lisk-sdk';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ModuleEndpointContext } from 'lisk-framework';
 import { address as cryptoAddress } from '@liskhq/lisk-cryptography';
-import { UserAccountJSON, Store, UserAccount } from '../types';
-import { accountStoreSchema } from '../schemas';
+import {
+  UserAccountJSON,
+  Store,
+  UserAccount,
+  User,
+  UserJSON,
+} from '../types';
+import { accountStoreSchema, userStoreSchema } from '../schemas';
 
 export const getAccount = async (
   context: ModuleEndpointContext,
@@ -31,4 +37,31 @@ export const getAccount = async (
   const accountData = await userAccountStore.get(context, addressBuffer);
   const accountJSON: UserAccountJSON = codec.toJSON(accountStoreSchema, accountData);
   return accountJSON;
+};
+
+export const getUser = async (
+  context: ModuleEndpointContext,
+  userSubStore: Store<User>,
+): Promise<UserJSON> => {
+  const { userID } = context.params;
+
+  let query: Buffer;
+
+  if (Buffer.isBuffer(userID)) {
+    query = userID;
+  } else if (typeof userID === 'string') {
+    query = Buffer.from(userID, 'hex');
+  } else {
+    throw new Error('Parameter userID must be a string or a buffer.');
+  }
+
+  const userExists = await userSubStore.has(context, query);
+
+  if (!userExists) {
+    throw new Error(`No user with id ${query.toString('hex')} found.`);
+  }
+
+  const userData = await userSubStore.get(context, query);
+  const userJSON: UserJSON = codec.toJSON(userStoreSchema, userData);
+  return userJSON;
 };
