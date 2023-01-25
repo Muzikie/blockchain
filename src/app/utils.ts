8cd7424e0fdb7eb2e44b607249792cd362e8681c
Ali Haghighatkhah <ali@lightcurve.io>
2023-01-25 21:34:45 +0100

    Use md5 hash of tx to generate ID

---
+++ src/app/utils.ts
@@ -0,0 +1,26 @@
+import { createHash } from 'crypto';
+import { ed } from '@liskhq/lisk-cryptography';
+import { codec } from '@liskhq/lisk-codec';
+import { Transaction } from './types';
+import { baseTransactionSchema } from './schemas';
+
+export const getNodeForName = <T>(transaction: Transaction<T>): Buffer => {
+  const txBytes = codec.encode(baseTransactionSchema, transaction);
+  const content = txBytes.toString('hex');
+  return createHash('md5').update(content).digest();
+};
+
+export const verifyHash = (signature: Buffer, message: Buffer, publicKey: Buffer) => {
+  let isCorrect = false;
+
+  try {
+    isCorrect = ed.verifyMessageWithPublicKey({
+      message: message.toString('hex'),
+      publicKey,
+      signature,
+    });
+  } catch (e) {
+    isCorrect = false;
+  }
+  return isCorrect;
+};
