f4b56b854bcfee8d87a70f58a8c4b1dd472dfd56
curvesy <sina@muzikie.com>
2023-10-20 20:13:43 +0330

    Add two new method to badges methods(getWinningAnchorsForDate and updateBadgesForDate)

--- src/app/modules/badge/method.ts
+++ src/app/modules/badge/method.ts
@@ -1,7 +1,7 @@
 import { BaseMethod } from 'lisk-sdk';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { ModuleEndpointContext } from 'lisk-framework';
-import { BadgeAccountJSON, BadgeJSON, Badge, BadgeAccount, Store } from './types';
+import { ModuleEndpointContext, MethodContext } from 'lisk-framework';
+import { BadgeAccountJSON, BadgeJSON, Badge, BadgeAccount, Store} from './types';
 import { BadgeAccountStore } from './stores/badgeAccount';
 import { BadgeStore } from './stores/badge';
 import { getAccount, getBadge } from './controllers/query';
@@ -16,4 +16,45 @@ export class BadgeMethod extends BaseMethod {
     const badgeSubStore = this.stores.get(BadgeStore);
     return getBadge(context, badgeSubStore as Store<Badge>);
   }
+  
+
+  public async getWinningAnchorsForDate(context: ModuleEndpointContext, date: string): Promise<Buffer[]> {
+    const BADGE_TYPE = 'anchor_of_the_day'
+    const badgeStore = this.stores.get(BadgeStore);
+    
+    const badgeIDs = [1,2,3].map((rank) => `${date}-${rank}-${BADGE_TYPE}`);
+
+    const badges = await Promise.all(
+      badgeIDs.map(async (badgeID) => badgeStore.get(context, Buffer.from(badgeID, 'hex'))),
+    );
+  
+    return badges.map((badge) => badge.anchorID);
+  }
+  
+  public async updateBadgesForDate(context: MethodContext, date: string, updatedWinningAnchors: Array<Buffer>) {
+    const BADGE_TYPE = 'anchor_of_the_day'
+
+    const badgeIDs = [1,2,3].map((rank) => `${date}-${rank}-${BADGE_TYPE}`);
+    const badgeSubStore = this.stores.get(BadgeStore);
+  
+    const badges = await Promise.all(
+      badgeIDs.map(async (badgeID) => badgeSubStore.get(context,  Buffer.from(badgeID, 'hex'))),
+    );
+  
+    for (const badge of badges) {
+      const newAwardee  = updatedWinningAnchors.shift();
+      const updatedBadge = {
+        ...badge,
+        ...newAwardee,
+      }
+      await badgeSubStore.set(context, badge.anchorID, updatedBadge);
+    }
+  
+    return true;
+  }
+  
 }
+
+
+
+
