55187842a8817412099da7535dd55bd9ccfddfe9
reyraa <ali@muzikie.com>
2023-10-20 18:42:43 +0200

    Import context types from lisk framework

--- src/app/modules/badge/endpoint.ts
+++ src/app/modules/badge/endpoint.ts
@@ -1,19 +1,19 @@
 import { BaseEndpoint } from 'lisk-sdk';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ModuleEndpointContext } from 'lisk-framework';
-import { BadgeAccountJSON, BadgeJSON, Badge, BadgeAccount, Store } from './types';
+import { BadgeAccountJSON, BadgeJSON } from './types';
 import { BadgeAccountStore } from './stores/badgeAccount';
 import { BadgeStore } from './stores/badge';
 import { getAccount, getBadge } from './controllers/query';
 
 export class BadgeEndpoint extends BaseEndpoint {
   public async getAccount(context: ModuleEndpointContext): Promise<BadgeAccountJSON> {
-    const badgeAccountSubStore = this.stores.get(BadgeAccountStore);
-    return getAccount(context, badgeAccountSubStore as Store<BadgeAccount>);
+    const badgeAccountStore = this.stores.get(BadgeAccountStore);
+    return getAccount(context, badgeAccountStore);
   }
 
   public async getBadge(context: ModuleEndpointContext): Promise<BadgeJSON> {
-    const badgeSubStore = this.stores.get(BadgeStore);
-    return getBadge(context, badgeSubStore as Store<Badge>);
+    const badgeStore = this.stores.get(BadgeStore);
+    return getBadge(context, badgeStore);
   }
 }
